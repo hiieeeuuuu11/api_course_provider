3de57f8f5cc45bb9fd0b8a3625cf77ef1d186e46
Nguyễn Trung Hiếu <93122716+hiieeeuuuu11@users.noreply.github.com>
2024-10-26 15:31:47 +0700

    refactor v5

--- src/main/java/com/example/api_course_producer/ApiCourseProducerApplication.java
+++ src/main/java/com/example/api_course_producer/ApiCourseProducerApplication.java
@@ -1,12 +1,7 @@
 package com.example.api_course_producer;
 
-import com.example.api_course_producer.service.MailService;
-import jakarta.mail.MessagingException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class ApiCourseProducerApplication {
--- src/main/java/com/example/api_course_producer/controller/course/LessonController.java
+++ src/main/java/com/example/api_course_producer/controller/course/LessonController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/upload/lesson")
+@RequestMapping("/lesson")
 @CrossOrigin("*")
 @RequiredArgsConstructor
 public class LessonController {
